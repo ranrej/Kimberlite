9f7ecfafd1f839ee11a29a04b7086e2d9ace7295
ranrej <rania.rejdal@ontariotechu.net>
2024-07-14 08:43:42 -0400

    App clean created

---
+++ Kimberlite/Kimberlite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kimberlite
+//
+//  Created by Rania Rejdal on 2024-07-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kimberlite/Kimberlite/KimberliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  KimberliteApp.swift
+//  Kimberlite
+//
+//  Created by Rania Rejdal on 2024-07-14.
+//
+
+import SwiftUI
+
+@main
+struct KimberliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
